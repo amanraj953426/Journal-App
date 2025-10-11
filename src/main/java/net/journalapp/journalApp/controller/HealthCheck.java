e0dc40720062eaa5797cbb47e6906a4be41bf3ab
deadpool <deadpool@kali.kali>
2025-10-12 00:15:48 +0530

    add heath check

--- src/main/java/net/journalapp/journalApp/controller/HealthCheck.java
+++ src/main/java/net/journalapp/journalApp/controller/HealthCheck.java
@@ -1,4 +1,4 @@
-package net.journalapp.controller;
+package net.journalapp.journalApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
